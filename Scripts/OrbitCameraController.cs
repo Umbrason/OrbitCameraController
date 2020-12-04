0170d022fd74688081c55bf4b3bf06bb01fc7418
Umbrason <FabioKolligs@outlook.de>
2020-12-04 01:43:02 +0100

    Update OrbitCameraController.cs

--- Scripts/OrbitCameraController.cs
+++ Scripts/OrbitCameraController.cs
@@ -77,13 +77,13 @@ public class OrbitCameraController : MonoBehaviour
             desiredRotationSpeed.y = rotationInput.x;
             desiredRotationSpeed /= 10f;
             if (rotationSettings.easingBehaviour == RotationSettings.RotationEasing.Always)
-                rotationSpeed = Vector2.Lerp(desiredRotationSpeed, rotationSpeed, 1 - Mathf.Pow(1f / rotationSettings.smoothness, Time.deltaTime));
+                rotationSpeed = Vector2.Lerp(rotationSpeed, desiredRotationSpeed, 1 - Mathf.Pow(1f / rotationSettings.smoothness, Time.deltaTime));
             else
                 rotationSpeed = desiredRotationSpeed;
         }
         else if (rotationSettings.easingBehaviour != RotationSettings.RotationEasing.None)
         {
-            rotationSpeed = Vector2.Lerp(Vector2.zero, rotationSpeed, 1 - Mathf.Pow(1f / rotationSettings.smoothness, Time.deltaTime));
+            rotationSpeed = Vector2.Lerp(rotationSpeed, Vector2.zero, 1 - Mathf.Pow(1f / rotationSettings.smoothness, Time.deltaTime));
         }
         else rotationSpeed = Vector2.zero;
 
